1cab622a937248700d7104175c3833ac2d65c74d
vikas sethi <vikassethi2000@gmail.com>
2020-10-27 16:20:54 +0530

    arranged the files the sequential manner

--- bmi1.py
+++
@@ -1,7 +0,0 @@
-#This is a BMI calculator
-print("This is a BMI calculator")
-name=str(input("enter your name"))
-mass=int(input("enter your weight in kg's"))
-height=float(input("enter your height in meters!"))
-bmi=mass/(height*height)
-print(f'Hey {name},your height is {height},your weight is {mass} and your BMI is {bmi}')
