05e6d40c67e9c28a2c499d839e3a66dce18d7c45
jt1011 <jatintanwar16@gmail.com>
2020-10-29 00:34:58 +0530

    Day 3 Assignment Submitted By jatin tanwar

---
+++ Day3/Jatin Tanwar/jatin_q1.py
@@ -0,0 +1,25 @@
+# This is a simple program to run simple operations like addition , multiplication , division and subtraction 
+
+a  = int(input ("Please Enter The First Number"))
+
+b = int(input("Please ENter The Second Number"))
+
+x = (a+b) 
+x = float(x)
+print ("this is the result of addition ")
+print (x)
+   
+y = (a-b)
+y = float(y)
+print ("this is the result of subtraction ")
+print (y)
+
+z = (a*b)
+z = float(z)
+print ("this is the result of multiplication ")
+print (z)
+
+k = (a/b)
+k = float(k)
+print ("this is the result of division ")
+print (k)
