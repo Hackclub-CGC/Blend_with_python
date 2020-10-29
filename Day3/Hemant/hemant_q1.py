8a0c684a12484db1a2c6ec43183fd691b737f0ba
Hemant Kumar Singh <ksingh1617@gmail.com>
2020-10-29 15:12:46 +0530

    assingment added day 3

---
+++ Day3/Hemant/hemant_q1.py
@@ -0,0 +1,7 @@
+x=float(input("Enter first number:"))
+y=float(input("Enter second number:"))
+
+print(f"Sum of {x} and {y} is:{x+y}")
+print(f"Product of {x} and {y} is:{x*y}")
+print(f"Difference of {x} and {y} is:{x-y}")
+print(f"Reminder of {x} and {y} is:{x%y}")
